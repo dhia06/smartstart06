3616513d75d78f7b6aa4736008e390b2670f110d
MohamedAmineNjah <44654432+MohamedAmineNjah@users.noreply.github.com>
2019-12-03 20:51:12 +0100

    03/12/2019
    Amine

---
+++ avant ckeditor et ajax/src/MainBundle/Entity/Evenement.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Evenement
+ *
+ * @ORM\Table(name="evenement", indexes={@ORM\Index(name="id_categorie", columns={"id_categorie"})})
+ * @ORM\Entity
+ */
+class Evenement
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255, nullable=false)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lieu", type="string", length=255, nullable=false)
+     */
+    private $lieu;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, nullable=false)
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_event", type="date", nullable=false)
+     */
+    private $dateEvent;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="nbplace", type="integer", nullable=false)
+     */
+    private $nbplace;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom_image", type="string", length=255, nullable=false)
+     */
+    private $nomImage;
+
+    /**
+     * @var \Categorie
+     *
+     * @ORM\ManyToOne(targetEntity="Categorie")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_categorie", referencedColumnName="id")
+     * })
+     */
+    private $idCategorie;
+
+
+}
+
